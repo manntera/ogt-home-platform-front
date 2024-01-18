85c1c9c2c6b3e97693e5ae54503edb5b90d7ae34
manntera <studio@manntera.com>
2024-01-18 21:33:33 +0900

    今度こそタップでTooltipが表示される様になったと思う

--- src/components/Main/DataViewer/TileItem.tsx
+++ src/components/Main/DataViewer/TileItem.tsx
@@ -13,8 +13,22 @@ type Props = {
 };
 
 export const TileItem: React.FC<Props> = ({ sizex, sizey, text, toolTipText, color, outlineColor, OutlineSize, style, onClick }) => {
+    const [open, setOpen] = useState(false);
+
+    const onTouchEnd = (event: React.TouchEvent<HTMLDivElement>) => {
+        setOpen(true);
+        setTimeout(() => setOpen(false), 5000);
+    };
+
     return (
-        <Tooltip title={toolTipText ?? ''} placement="top-end" enterTouchDelay={0}>
+        <Tooltip
+            title={toolTipText ?? ''}
+            placement="top-end"
+            open={open}
+            disableFocusListener
+            disableHoverListener
+            disableTouchListener
+        >
             <Box
                 sx={{
                     width: sizex,
@@ -27,9 +41,14 @@ export const TileItem: React.FC<Props> = ({ sizex, sizey, text, toolTipText, col
                 }}
                 style={style}
                 onClick={onClick}
+                onTouchEnd={onTouchEnd}
             >
                 <Typography sx={{ userSelect: 'none' }}>{text}</Typography>
             </Box>
         </Tooltip>
     );
 };
+function useState(arg0: boolean): [any, any] {
+    throw new Error('Function not implemented.');
+}
+
