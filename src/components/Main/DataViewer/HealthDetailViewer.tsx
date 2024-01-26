1d018e303c7124776fd1e69a0c351f4b3f38e748
manntera <studio@manntera.com>
2024-01-26 21:56:47 +0900

    とある時間帯に登録された体調データの詳細を見る事が出来る様になりました。

--- src/components/Main/DataViewer/HealthDetailViewer.tsx
+++ src/components/Main/DataViewer/HealthDetailViewer.tsx
@@ -1,5 +1,19 @@
-import { usePostApi, HealthGetResponse, HealthApiUrl, HealthGetRequest } from "@/hooks/usePostApi";
-import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, List, ListItem, ListItemText } from "@mui/material";
+import {
+    usePostApi,
+    HealthGetResponse,
+    HealthApiUrl,
+    HealthGetRequest,
+} from "@/hooks/usePostApi";
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    List,
+    ListItem,
+    ListItemText,
+} from "@mui/material";
 import { useEffect, useState } from "react";
 
 type Props = {
@@ -10,16 +24,24 @@ type Props = {
     userId: string;
 };
 
-export const HealthDetailViewer: React.FC<Props> = ({ onClickClose, isOpenDialog, startUnixTime, endUnixTime, userId }) => {
+export const HealthDetailViewer: React.FC<Props> = ({
+    onClickClose,
+    isOpenDialog,
+    startUnixTime,
+    endUnixTime,
+    userId,
+}) => {
     const targetDate = new Date(startUnixTime * 1000);
     const targetMonth = targetDate.getMonth();
     const targetDay = targetDate.getDate();
     const targetHour = targetDate.getHours();
 
-    // 健康データの状態
     const [healthData, setHealthData] = useState<HealthGetResponse[]>([]);
 
-    const { submitData: submitGetHealth } = usePostApi<HealthGetRequest, HealthGetResponse[]>(HealthApiUrl + "get");
+    const { submitData: submitGetHealth } = usePostApi<
+        HealthGetRequest,
+        HealthGetResponse[]
+    >(HealthApiUrl + "get");
 
     const handleClose = () => {
         setHealthData([]);
@@ -34,7 +56,7 @@ export const HealthDetailViewer: React.FC<Props> = ({ onClickClose, isOpenDialog
             const request: HealthGetRequest = {
                 userId: userId,
                 startTime: startUnixTime,
-                endTime: endUnixTime
+                endTime: endUnixTime,
             };
 
             const res = await submitGetHealth(request);
@@ -47,27 +69,39 @@ export const HealthDetailViewer: React.FC<Props> = ({ onClickClose, isOpenDialog
     }, [startUnixTime, endUnixTime, userId, submitGetHealth, isOpenDialog]);
 
     return (
-        <Box sx={{ textAlign: 'center', mt: 2 }}>
-            <Dialog open={isOpenDialog} onClose={handleClose}>
-                <DialogTitle>
-                    {targetMonth}月{targetDay}日 {targetHour}時の健康情報
-                </DialogTitle>
-                <DialogContent>
-                    <List>
-                        {healthData.map((item, index) => (
+        <Dialog open={isOpenDialog} onClose={handleClose} fullWidth={true}>
+            <DialogTitle>
+                {targetMonth}月{targetDay}日 {targetHour}時の健康情報
+            </DialogTitle>
+            <DialogContent>
+                <List>
+                    {healthData.map((item, index) => {
+                        const date = new Date(item.timestamp * 1000);
+                        const hours = date.getHours();
+                        const minutes = date
+                            .getMinutes()
+                            .toString()
+                            .padStart(2, "0");
+                        return (
                             <ListItem key={index}>
                                 <ListItemText
-                                    primary={`スコア: ${item.healthScore}`}
-                                    secondary={`コメント: ${item.comment || 'なし'}Time: ${item.timestamp}`}
+                                    primary={`${hours}:${minutes}`}
+                                    secondary={`【体調】 ${
+                                        item.healthScore
+                                    }【コメント】${
+                                        item.comment || "コメントなし"
+                                    }`}
                                 />
                             </ListItem>
-                        ))}
-                    </List>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={onClickClose} color="primary">OK</Button>
-                </DialogActions>
-            </Dialog>
-        </Box>
+                        );
+                    })}
+                </List>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={onClickClose} color="primary">
+                    OK
+                </Button>
+            </DialogActions>
+        </Dialog>
     );
 };
