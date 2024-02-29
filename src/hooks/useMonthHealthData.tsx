11bbd70875e35570af6b8a2ee2a8b614319f5c9d
manntera <studio@manntera.com>
2024-02-29 16:55:35 +0900

    体調情報をGridで確認する際、月末日分を含めたリクエストになっていなかった問題を修正しました。

--- src/hooks/useMonthHealthData.tsx
+++ src/hooks/useMonthHealthData.tsx
@@ -26,7 +26,7 @@ export const useMonthHealthData = (year: number, month: number) => {
             const firstDayOfMonth = new Date(year, month - 1, 1);
             const lastDayOfMonth = new Date(year, month, 0);
             const startTime = Math.floor(firstDayOfMonth.getTime() / 1000);
-            const endTime = Math.floor(lastDayOfMonth.getTime() / 1000);
+            const endTime = Math.floor((lastDayOfMonth.getTime() + (24 * 60 * 60000)) / 1000);
             const request: HealthGetRequest = {
                 userId: currentUser.uid,
                 startTime: startTime,
